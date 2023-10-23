2dc77991f3dbfaa0d79da014f23ea27c7424bfcc
xue <xue515953749@gmail.com>
2023-10-23 08:06:31 -0500

    10/22

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
 import Employee from "./components/Employee";
 import { useState } from "react";
+import './index.css';
 function App() {
   const [role, setRole] = useState('dev');
 
   const showEmployees = true;
 
   return (
-    <div className="App">
+    <div className="App bg-red-300 text-3xl font-bold">
       
       {showEmployees ? (
         <>
 
-          <input type="text" 
+          <input className="bg-orange-400" type="text" 
           onChange={(a)=>{
             console.log(a.target.value);
             setRole(a.target.value);
           }}
           />;
-          <Employee name="caleb" role="intern"/>
+          <Employee  name="caleb" role="intern"/>
           <Employee role = {role}/>
           <Employee/>
           <Employee/>
+          <p className="bg-orange-200">You can not see the employees</p>
         </>
-       ) : ( <p>You can not see the employees</p>
+       ) : ( <p className="bg-orange-400">You can not see the employees</p>
        )}
     </div>
   );
