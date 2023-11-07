b5d4213afd29b49216527face5b026edfcd8c2e7
Yongjie Xue <yxue9@huskers.unl.edu>
2023-11-06 20:00:37 -0600

    fix some 404 display

--- src/pages/Customer.js
+++ src/pages/Customer.js
@@ -6,16 +6,30 @@ export default function Customer() {
     const { id } = useParams();
     const [customer, setCustomer] = useState();
     const [notfound, setNotfound] = useState();
+    const [error, setError] = useState();
     const [tempCustomer, setTempCustomer] = useState();
     const [changed, setChanged] = useState(false);
     const navigate = useNavigate();
+
     useEffect(() => {
         // console.log('setChanged', changed);
         // console.log('tmpcustomer', tempCustomer);
         // console.log('customer', customer);
+
     })
     useEffect(() => {
-        console.log("useEffect")
+        if (!customer) return;
+        if (!tempCustomer) return;
+        let equal = true;
+        if (customer.name !== tempCustomer.name || customer.industry !== tempCustomer.industry) {
+            equal = false;
+        }
+        setChanged(!equal);
+        // console.log("call compare", equal);
+    }, [customer, tempCustomer]);
+
+    useEffect(() => {
+        // console.log("useEffect")
         const url = baseURL + 'api/customers/' + id
         fetch(url)
             .then((response) => {
@@ -23,12 +37,18 @@ export default function Customer() {
                     navigate('/404');
                     setNotfound(true);
                 }
+                if (!response.ok) {
+                    throw new Error(' something went wrong')
+                }
+
+
                 return response.json()
             })
             .then((data) => {
                 // console.log(data.customer.name)
                 setCustomer(data.customer);
                 setTempCustomer(data.customer);
+                setError(undefined);
             })
             .catch((e) => {
                 console.log("catch: " + e.message);
@@ -37,6 +57,7 @@ export default function Customer() {
     function deleteCustomer() {
         console.log('deleting..');
     }
+
     function updateCustomer() {
         const url = baseURL + 'api/customers/' + id;
         fetch(
@@ -47,16 +68,19 @@ export default function Customer() {
             },
             body: JSON.stringify(tempCustomer)
         }).then((response) => {
-            console.log(response.status)
+            if (!response.ok) {
+                throw new Error('Something went wrong');
+            }
             return response.json()
         }
         ).then((data) => {
-
-            setChanged(false)
-            // setCustomer({...tempCustomer})
-            setCustomer(data.customer)     // data.customer and ...tempCustomer they are all lists
-            // console.log("indata: ",{...tempCustomer})
-        }).catch()
+            setChanged(false);
+            setCustomer(data.customer);    // data.customer and ...tempCustomer they are all lists
+            setError(undefined);
+        }).catch((e) => {
+            console.log(e);
+            setError(e.message);
+        });
     }
     return (
         <>
@@ -68,12 +92,11 @@ export default function Customer() {
                         type='text'
                         value={tempCustomer.name}
                         onChange={(e) => {
+                            setChanged(true);
                             setTempCustomer({
                                 ...tempCustomer,
                                 name: e.target.value
                             });
-                            setChanged(true);
-
                         }}
                     />
                     <input
@@ -81,10 +104,13 @@ export default function Customer() {
                         type='text'
                         value={tempCustomer.industry}
                         onChange={(e) => {
+                            setChanged(true);
                             setTempCustomer({
                                 ...tempCustomer,
                                 industry: e.target.value
-                            })
+                            });
+                            // compareCustomers();
+
 
                         }}
                     />
@@ -108,26 +134,32 @@ export default function Customer() {
                             </>
                         ) :
                         (<p>not Changed </p>)}
+
+
+                    <button onClick={(e) => {
+                        const url = baseURL + 'api/customers/' + id;
+                        fetch(url, {
+                            method: 'DELETE',
+                            headers: {
+                                'Content-Type': 'application/json'
+                            }
+                        }).then((response) => {
+
+                            if (!response.ok) {
+                                throw new Error('Something went wrong');
+                            }
+                            setError(undefined);
+                            navigate('/customers');
+                        }).catch((e) => {
+                            console.log(e);
+                            setError(true);
+                        })
+                    }}>Delete
+                    </button>
+
                 </div>
-            ) : (<p> </p>)}
-
-            <button onClick={(e) => {
-                const url = baseURL + 'api/customers/' + id;
-                fetch(url, {
-                    method: 'DELETE',
-                    headers: {
-                        'Content-Type': 'application/json'
-                    }
-                }).then((response) => {
-
-                    if (!response.ok) {
-                        throw new Error('Something went wrong');
-                    }
-                    navigate('/customers');
-                }).catch((e) => {
-                    console.log(e);
-                })
-            }}>Delete</button>
+            ) : null}
+            {error ? (<p>{error}</p>) : null}
             <br />
             <Link to='/customers'>Go Back </Link>
 
